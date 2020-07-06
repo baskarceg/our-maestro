ac0df14fe5a5c320c69e35bf9aeb62ca30a23497
baskarceg <baskarrathinavel11834@gmail.com>
2020-07-06 21:12:53 +0530

    100% Done

--- src/containers/HerWorld/HerWorld.js
+++ src/containers/HerWorld/HerWorld.js
@@ -13,12 +13,12 @@ class HerWorld extends Component {
                     <img src="https://i.ibb.co/d5Sm3NK/amma1.jpg" alt="Amma"></img>
                     <div className={classes.Paragraph}>
                         <p>
-                            Anyone who knows Roshini even a little bit , can definitely say who the most important person in the world is to her. Her mom. Amma. <br></br> <br></br>Amma is everything to her. Anything she does , she does so by keeping the happiness of amma in her mind. If she thinks that something that she's going to do is gonna upset amma , she drops it without a second thought. <br></br> <br></br>If amma is happy, Roshini is happy. Amma is always in her heart , mind and thought.<br></br><br></br>These words may seem exaggerated. You may even say that every daughter loves her mom. But the love Roshini has for amma is definitely over and above that.
+                        Anyone who knows Roshini even a little bit , can definitely say who the most important person in the world is to her. Her mom. Amma. <br/><br/>Her mom is everything to her. Anything she does , she does so by keeping the happiness of her mom in her mind. If something she may do has even the slightest possibility to upset her mom , she’d never even imagine doing it.<br/><br/>Her mom’s happiness is the most important to her, and nothing can ever gain more importance over it.Even when she gives a hell lot of importance to her friends, it does not even come close to her love for Amma. If Amma is happy, Roshini is happy. Amma is always in her heart , mind and thoughts.<br/><br/>These words may seem exaggerated. You may even say that every daughter loves her mom. But if you are part of Roshini’s are Amma’s circle you’ll know that it’s an understatement.
                         </p>
                     </div>
                 </div>
                 <div className={classes.Heading}>
-                    <h3 >The Chemistry</h3>
+                    <h3 >Their Chemistry</h3>
                     <hr></hr>
                 </div>
                 <div className={classes.Content}>
@@ -27,21 +27,21 @@ class HerWorld extends Component {
 
                     <div className={classes.Paragraph}>
                         <p>
-                            Anyone who knows Roshini even a little bit , can definitely say who the most important person in the world is to her. Her mom. Amma. <br></br> <br></br>Amma is everything to her. Anything she does , she does so by keeping the happiness of amma in her mind. If she thinks that something that she's going to do is gonna upset amma , she drops it without a second thought. <br></br> <br></br>If amma is happy, Roshini is happy. Amma is always in her heart , mind and thought.<br></br><br></br>These words may seem exaggerated. You may even say that every daughter loves her mom. But the love Roshini has for amma is definitely over and above that.
+                        The relationship between Roshini and Amma , is something every mother and every daughter long for. They are like two best friends who are inseparable.<br/><br/>Everything I said about Rosh's love for Amma can be said about Amma's love for Rosh.<br/><br/>Roshini is everything to Amma in the same way Amma is everything to Rosh.Her world is Rosh. Amma always wishes and provides the best for our Maestro. If the person a child grows into reflects how well the child is brought up , we all need to take notes on how to nurture a child into a responsible and empathising woman like Roshini from Amma. <br/><br/>It’s really not a surprise that anything you can say about Rosh’s character or Amma’s character , you can incidentally say about the other. That is how much they are similar. They both have such a warm personality, always put themselves in the shoes of another human being , can’t even imagine them hurting another soul  and always , always , full of love.
                         </p>
                     </div>
                     <img class={classes.Horizondal} src="https://i.ibb.co/PrTj6xk/amma3.jpg" alt="Amma"></img>
 
                 </div>
                 <div className={classes.Heading}>
-                    <h3 >Why we love them?</h3>
+                    <h3 >Why we love this pair?</h3>
                     <hr></hr>
                 </div>
                 <div className={classes.Content}>
                     <img src="https://i.ibb.co/PrTj6xk/amma3.jpg" alt="Amma"></img>
                     <div className={classes.Paragraph}>
                         <p>
-                            Anyone who knows Roshini even a little bit , can definitely say who the most important person in the world is to her. Her mom. Amma. <br></br> <br></br>Amma is everything to her. Anything she does , she does so by keeping the happiness of amma in her mind. If she thinks that something that she's going to do is gonna upset amma , she drops it without a second thought. <br></br> <br></br>If amma is happy, Roshini is happy. Amma is always in her heart , mind and thought.<br></br><br></br>These words may seem exaggerated. You may even say that every daughter loves her mom. But the love Roshini has for amma is definitely over and above that.
+                        This mother daughter duo is something we all hold close to our hearts. It is not a common thing that when you talk about a person , you somehow also start talking about their parents. This is the case of Roshini and Amma. You can’t talk about one person without talking about the other.<br/><br/>When you ask about Rosh, you can’t describe her without talking about her sincere love towards her mom. And you can’t describe Amma, without talking about how she has given her heart and soul into the health , well-being and happiness of Roshini.<br/><br/>Every-time I watch a movie, where the relationship of a mother and son or mother and daughter is so pure , or when a mother’s only thought is the betterment of her daughter’s life, they immediately come to my mind. That is why I love their relationship.
                         </p>
                     </div>
 
